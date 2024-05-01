6cfd18d0d60ae61be0402e3686b7525960735b82
Enoughrise <148502253+Enoughrise@users.noreply.github.com>
2024-05-01 18:37:18 +0700

    Update config.h

--- include/newb/config.h
+++ include/newb/config.h
@@ -98,7 +98,6 @@
 #define NL_PLANTS_WAVE 0.04    // [toggle] 0.02 gentle ~ 0.4 violent
 #define NL_LANTERN_WAVE 0.16   // [toggle] 0.05 subtle ~ 0.4 large swing
 #define NL_WAVE_SPEED 2.8      // 0.5 slow wave ~ 5.0 very fast wave
-#define NL_BOTTOM_PLANTS_SHADOW 1.4   // [toggle] 0.5 subtle - 3.0 dark
 //#define NL_EXTRA_PLANTS_WAVE // [toggle] !dont use! wave using texture coords (1.20.40 vanilla)
 
 /* Water */
